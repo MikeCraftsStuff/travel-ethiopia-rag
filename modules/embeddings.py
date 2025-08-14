f126614da961017cd91e9031a1d701393b2d1dfd
Mikiyas Sintayehu <mikiyas.sintayehu.sh@gmail.com>
2025-08-14 18:45:42 +0300

    Update embeddings.py

--- modules/embeddings.py
+++ modules/embeddings.py
@@ -1,1 +1,8 @@
 
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+def create_embeddings(documents):
+    embeddings = OpenAIEmbeddings()
+    vector_store = FAISS.from_documents(documents, embeddings)
+    return vector_store
