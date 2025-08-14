0502096ace7b7c29958832322d20125b2c891a2c
Mikiyas Sintayehu <mikiyas.sintayehu.sh@gmail.com>
2025-08-14 20:23:16 +0300

    Update embeddings.py

--- modules/embeddings.py
+++ modules/embeddings.py
@@ -1,8 +1,7 @@
-
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 
 def create_embeddings(documents):
-    embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     vector_store = FAISS.from_documents(documents, embeddings)
     return vector_store
