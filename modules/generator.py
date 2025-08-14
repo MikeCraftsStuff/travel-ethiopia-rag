d018452ef9fb9430fb407985a15b6d5300ab0637
Mikiyas Sintayehu <mikiyas.sintayehu.sh@gmail.com>
2025-08-14 18:47:01 +0300

    Update generator.py

--- modules/generator.py
+++ modules/generator.py
@@ -1,1 +1,17 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
+def generate_answer(query, relevant_chunks):
+    context = "\n".join([str(chunk.page_content) for chunk in relevant_chunks])
+    template = """You are a travel guide for Ethiopia.
+    Use the following context to answer the question:
+    {context}
+
+    Question: {question}
+    Answer:"""
+    prompt = PromptTemplate(input_variables=["context", "question"], template=template)
+    llm = ChatOpenAI(temperature=0)
+    chain = LLMChain(llm=llm, prompt=prompt)
+    answer = chain.run({"context": context, "question": query})
+    return answer
